598e4034ba1b96bbadb4df6dca2d894852f439d0
anazardetto <163781829+anazardetto@users.noreply.github.com>
2025-06-02 07:54:57 -0300

    Delete formulario_2.php

--- formulario_2.php
+++
@@ -1,9 +0,0 @@
-<form action="recebe.php" method="post">
-    Nome: <input type="text" name="nome">
-    <input type="submit" value="Enviar">
-</form>
-
-<?php
-$nome = $_POST['nome'] ?? 'Desconhecido';
-echo"Olá, $nome! Formulário recebido na nuvem.";
-?>
